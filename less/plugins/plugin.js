1e443a3f631c5585986896199913054d7de775a5
lishiwen <lishiwen@wclound.cn>
2022-07-16 16:26:55 +0800

    refactor: js

--- less/plugins/plugin.js
+++
@@ -1,8 +0,0 @@
-// const less = require("less");
-module.exports = {
-  install: function (less, pluginManager, functions) {
-    functions.add("sum", function (nodeLeft, nodeRight) {
-      return Number(nodeLeft.value) + Number(nodeRight.value) + "px";
-    });
-  },
-};
--- less/plugins/realpx.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  install: function (less, pluginManager, functions) {
-    functions.add("realpx", function (nodeLeft, nodeRight) {
-      console.log("realpx", nodeLeft, nodeRight, less);
-      return false;
-    });
-  },
-};
