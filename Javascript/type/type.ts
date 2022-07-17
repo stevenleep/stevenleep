cd5f1ebc0ecd21ff6d9ea68c5a2c67d829859ae3
lishiwen <lishiwen@wclound.cn>
2022-07-17 18:03:25 +0800

    feat: type typeof

--- Javascript/type/type.ts
+++ Javascript/type/type.ts
@@ -1,2 +1,1 @@
-const a = [];
-a instanceof Array;
+import "./instanceof";
---
+++ Javascript/type/typeof.ts
@@ -0,0 +1,2 @@
+// ref: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/typeof
+// typeof操作符返回一个字符串，表示 未经计算的操作数 的类型
