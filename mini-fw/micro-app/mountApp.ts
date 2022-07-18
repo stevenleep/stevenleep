e5ac1e04c5411391d009edf5dd50dea52d2efe9a
lishiwen <lishiwen@wclound.cn>
2022-07-18 10:55:50 +0800

    feat: remove old microapp

--- mini-fw/micro-app/mountApp.ts
+++
@@ -1,9 +0,0 @@
-import { App, OptionalLifeCycle } from "./mainApp";
-
-export function mountApp(apps: App[], lifecycle: OptionalLifeCycle) {
-    createApp(apps, lifecycle);
-}
-
-export function createApp(apps: App[], lifecycle: OptionalLifeCycle) {
-
-}
